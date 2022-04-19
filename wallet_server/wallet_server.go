a16fde008dc9e106dfbed30f72ac5a712f3eee74
Rodrigo <rodrigo.romero@mercadolibre.com>
2022-04-19 17:06:59 -0300

    problem fixed

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -99,7 +99,7 @@ func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Reque
 		m, _ := json.Marshal(bt)
 		buf := bytes.NewBuffer(m)
 
-		resp, _ := http.Post(ws.Gateway()+"/transactions", "application/json", buf)
+		resp, err := http.Post(ws.Gateway()+"/transactions", "application/json", buf)
 		if resp.StatusCode == 201 {
 			io.WriteString(w, string(utils.JsonStatus("success")))
 			return
